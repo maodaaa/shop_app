75158a4e675ced718841602e42730c2b2d7f8dea
maodaaa <gmaorid@gmail.com>
2022-03-25 22:37:18 +0700

    babel/eslint fix eror

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;

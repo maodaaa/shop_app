2ffb6118816c4fd772233b16ad8e262c37005814
maodaaa <gmaorid@gmail.com>
2022-03-30 17:48:53 +0700

    add home page, navbar, slider image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import Slider from '../components/Slider';
 import React from 'react';
+import Annoucement from '../components/Annoucement';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <div>
+    <div className="container">
+      <Annoucement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
